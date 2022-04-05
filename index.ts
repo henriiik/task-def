e9d3d6c5a2edcac229c1c8f7ed52c173b57947e6
Henrik Sjööh <henriiik@gmail.com>
2022-04-05 07:24:08 +0200

    add example

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as awsnative from "@pulumi/aws-native";
+
+const taskDef = new awsnative.ecs.TaskDefinition(
+	`task-def`,
+	{
+		containerDefinitions: [
+			1, 2, 3
+		].map<awsnative.types.input.ecs.TaskDefinitionContainerDefinitionArgs>(
+			(n) => ({
+				name: `task_${n}`,
+				image: `image_${n}`,
+				memory: Math.floor(Math.random() * 10 + 1)
+			})
+		)
+	},
+	{ replaceOnChanges: ["*"] }
+);
+
+export const containerDefinitions = taskDef.containerDefinitions;
